e1f7865a2875a28cffa13ab276af3b29690777f4
centek134 <centek134@wp.pl>
2021-11-06 14:57:56 +0100

    making skeleton app, testing api

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+import styled from 'styled-components';
+import Filters from './components/Filters';
 import './App.css';
 
-function App() {
+const Container = styled.main`
+  width: 100%;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+interface Recipe{
+  recipe:{
+    id:number,
+    title:string,
+    image:string
+  }[]
+}
+
+const App = () => {
+
+    const [recipes,setRecipes] = useState<Recipe["recipe"]>([])
+  
+  useEffect(() => {
+    fetch("https://api.spoonacular.com/recipes/complexSearch?&cuisine=Italian,American&number=20&apiKey=2f82075fb2ab456f8f690ee0710297d5",{
+      method:"GET"
+    }).then( response => response.json())
+    .then(data => {
+      console.log("Data", data);
+      setRecipes(data.results);
+      console.log("myrecipes list",recipes)
+    }).catch(err => console.log(err));
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Filters/>
+      <div>
+
+      {recipes.map(rec => {
+        return(
+          <React.Fragment>
+          <p>{rec.title}</p>
+          <img src={rec.image} width={50} height={50} alt=''/>
+          <p>{rec.id}</p>
+          </React.Fragment>
+        )
+      })}
+      </div>
+    </Container>
   );
 }
 
