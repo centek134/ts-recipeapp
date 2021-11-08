8e8dd42f6d640d6169e949b2133ee9e4c9e4de91
centek134 <centek134@wp.pl>
2021-11-08 19:57:08 +0100

    changes added functions now we can select specified dishes and based of selection we are fetching and displaying data on screen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import styled from 'styled-components';
-import Filters from './components/Filters';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Filters from "./components/Filters";
+import "./App.css";
 
 const Container = styled.main`
   width: 100%;
@@ -11,47 +11,65 @@ const Container = styled.main`
   justify-content: center;
 `;
 
-interface Recipe{
-  recipe:{
-    id:number,
-    title:string,
-    image:string
-  }[]
+interface Recipe {
+  recipe: {
+    id: number;
+    title: string;
+    image: string;
+  }[];
 }
 
 const App = () => {
+  const [recipes, setRecipes] = useState<Recipe["recipe"]>([]);
+  const [queryString, setQueryString] = useState<string>("");
 
-    const [recipes,setRecipes] = useState<Recipe["recipe"]>([])
-  
-  useEffect(() => {
-    fetch("https://api.spoonacular.com/recipes/complexSearch?&cuisine=Italian,American&number=20&apiKey=2f82075fb2ab456f8f690ee0710297d5",{
-      method:"GET"
-    }).then( response => response.json())
-    .then(data => {
-      console.log("Data", data);
-      setRecipes(data.results);
-      console.log("myrecipes list",recipes)
-    }).catch(err => console.log(err));
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[])
+  const foodSearch = () => {
+    fetch(
+      `https://api.spoonacular.com/recipes/complexSearch?&cuisine=${queryString}&number=20&apiKey=2f82075fb2ab456f8f690ee0710297d5`,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Data", data);
+        setRecipes(data.results);
+        console.log("myrecipes list", recipes);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const setFilters = (opt:string) => {
+    let str:string = queryString;
+    if(str.search(opt) === -1){
+      str+=opt  + ",";
+      setQueryString(str);
+    }
+    else{
+      str =str.replace(`${opt},`,"");
+      setQueryString(str);
+      console.log(queryString);
+    }
+  };
 
   return (
     <Container>
-      <Filters/>
+      <Filters btnClick={foodSearch}  filter = {setFilters}/>
       <div>
-
-      {recipes.map(rec => {
-        return(
-          <React.Fragment>
-          <p>{rec.title}</p>
-          <img src={rec.image} width={50} height={50} alt=''/>
-          <p>{rec.id}</p>
-          </React.Fragment>
-        )
-      })}
+        {recipes
+          ? recipes.map((rec,i) => {
+              return (
+                <React.Fragment key={i}>
+                  <p>{rec.title}</p>
+                  <img src={rec.image} width={50} height={50} alt="" />
+                  <p>{rec.id}</p>
+                </React.Fragment>
+              );
+            })
+          : null}
       </div>
     </Container>
   );
-}
+};
 
 export default App;
